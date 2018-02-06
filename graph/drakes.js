5ad65b40340008f270726462007a10e27ed5613b
begue.16 <begue.16@osu.edu>
2018-02-06 16:38:25 -0500

    more notes

---
+++ graph/drakes.js
@@ -0,0 +1,137 @@
+this.data = [
+        {
+            "name": "CSA November Showcase",
+            "start_time": "2018-11-01T18:00:00-0400",
+            "id": "1819822354756132"
+        },
+        {
+            "name": "CSA October Showcase",
+            "start_time": "2018-10-04T18:00:00-0400",
+            "id": "2033114123597959"
+        },
+        {
+            "name": "CSA September Showcase",
+            "start_time": "2018-09-06T18:00:00-0400",
+            "id": "147391515977387"
+        },
+        {
+            "name": "CSA August Showcase",
+            "start_time": "2018-08-02T18:00:00-0400",
+            "id": "164822507505077"
+        },
+        {
+            "name": "CSA July Showcase",
+            "start_time": "2018-07-05T18:00:00-0400",
+            "id": "147418825959451"
+        },
+        {
+            "name": "CSA June Showcase",
+            "start_time": "2018-06-07T18:00:00-0400",
+            "id": "124625658347119"
+        },
+        {
+            "name": "CSA May Showcase",
+            "start_time": "2018-05-03T18:00:00-0400",
+            "id": "881709368652987"
+        },
+        {
+            "name": "Mister Moon/Marcus Alan Ward/Paper Morning/MIIR",
+            "start_time": "2018-04-20T20:00:00-0400",
+            "id": "150436212275755"
+        },
+        {
+            "name": "CSA April Showcase",
+            "start_time": "2018-04-05T18:00:00-0400",
+            "id": "144966506297840"
+        },
+        {
+            "name": "Shakti Shakedown",
+            "start_time": "2018-03-22T21:00:00-0400",
+            "id": "370257046759225"
+        },
+        {
+            "name": "Johnson Family Funk featuring the Jenny Flory band",
+            "start_time": "2018-03-09T21:00:00-0500",
+            "id": "143739472986604"
+        },
+        {
+            "name": "CSA March Showcase",
+            "start_time": "2018-03-01T18:00:00-0500",
+            "id": "379476905856632"
+        },
+        {
+            "name": "Jazz Wednesday w/ Joe Policastro Trio",
+            "start_time": "2018-02-28T20:00:00-0500",
+            "id": "1614657661929599"
+        },
+        {
+            "name": "Black River Gypsies w/ The Salty Caramels!",
+            "start_time": "2018-02-24T21:00:00-0500",
+            "id": "167400250529314"
+        },
+        {
+            "name": "Leaders of the Shift",
+            "start_time": "2018-02-22T21:00:00-0500",
+            "id": "323275531512976"
+        },
+        {
+            "name": "Osage // The WS Collective",
+            "start_time": "2018-02-17T21:00:00-0500",
+            "id": "197772250777538"
+        },
+        {
+            "name": "Terrance & Friends",
+            "start_time": "2018-02-16T21:00:00-0500",
+            "id": "1532745813469640"
+        },
+        {
+            "name": "Shakti Shakedown",
+            "start_time": "2018-02-15T21:00:00-0500",
+            "id": "1979970518684789"
+        },
+        {
+            "name": "Jazz Wednesday for Lovers w/Alex Burgoyne and Maxwell Button",
+            "start_time": "2018-02-14T20:00:00-0500",
+            "id": "1924674317861951"
+        },
+        {
+            "name": "Hayley Sabella / Hello Emerson / WYD",
+            "start_time": "2018-02-11T20:00:00-0500",
+            "id": "320946608401886"
+        },
+        {
+            "name": "Midnight Orchid + Robert Mason Trio 2.9.17",
+            "start_time": "2018-02-09T22:00:00-0500",
+            "id": "1652539091477226"
+        },
+        {
+            "name": "Jordan Kirk Vinyl Release // feat. Rowe / Small Songs / Friends",
+            "start_time": "2018-02-08T20:00:00-0500",
+            "id": "212674295948166"
+        },
+        {
+            "name": "Jazz Wednesday w/ Seth Daily (Tinsel Pig)",
+            "start_time": "2018-02-07T20:00:00-0500",
+            "id": "184104635519160"
+        },
+        {
+            "name": "Fo/Mo/Deep at Brothers Drake Meadery",
+            "start_time": "2018-02-03T21:00:00-0500",
+            "id": "880557528770010"
+        },
+        {
+            "name": "The Big Badd at Brothers Drake",
+            "start_time": "2018-02-02T21:00:00-0500",
+            "id": "208570829690597"
+        },
+        {
+            "name": "CSA February Showcase",
+            "start_time": "2018-02-01T18:00:00-0500",
+            "id": "148896332496741"
+        },
+        {
+            "name": "Jazz Wednesday with On a Limb",
+            "start_time": "2018-01-31T20:00:00-0500",
+            "id": "637465669977767"
+        }
+    ]
---
+++ graph/rumba.js
@@ -0,0 +1,232 @@
+this.data = [
+    {
+        "name": "Agent Orange w/ Fea",
+        "start_time": "2018-08-24T20:00:00-0400",
+        "id": "393037634472687"
+    },
+    {
+        "name": "The Blasters w/ Clownvis Presley",
+        "start_time": "2018-06-23T21:00:00-0400",
+        "id": "1545348768874098"
+    },
+    {
+        "name": "The Native Howl",
+        "start_time": "2018-05-20T19:00:00-0400",
+        "id": "211442966099532"
+    },
+    {
+        "name": "Patrick Sweany",
+        "start_time": "2018-05-11T21:00:00-0400",
+        "id": "1159850697451770"
+    },
+    {
+        "name": "Richard Buckner",
+        "start_time": "2018-04-29T19:30:00-0400",
+        "id": "138914576902143"
+    },
+    {
+        "name": "Tyrone Wells with special guest Gabe Dixon",
+        "start_time": "2018-04-20T19:00:00-0400",
+        "id": "530497373991626"
+    },
+    {
+        "name": "Shawn James",
+        "start_time": "2018-04-19T19:30:00-0400",
+        "id": "324978358005393"
+    },
+    {
+        "name": "Field Report w/ Campdogzz",
+        "start_time": "2018-04-08T19:00:00-0400",
+        "id": "770312886489255"
+    },
+    {
+        "name": "Common Deer",
+        "start_time": "2018-04-07T19:00:00-0400",
+        "id": "674106206069639"
+    },
+    {
+        "name": "Drift Mouth",
+        "start_time": "2018-03-31T21:00:00-0400",
+        "id": "184682728802997"
+    },
+    {
+        "name": "Daniel Ellsworth and the Great Lakes",
+        "start_time": "2018-03-29T20:00:00-0400",
+        "id": "162902831004009"
+    },
+    {
+        "name": "Keith Harkin",
+        "start_time": "2018-03-25T19:00:00-0400",
+        "id": "1948115388746664"
+    },
+    {
+        "name": "Way Yes Vinyl Release Show w/ CoastalDives",
+        "start_time": "2018-03-23T22:00:00-0400",
+        "id": "142343626438479"
+    },
+    {
+        "name": "St. Patrick's Day at Rumba Cafe w/ Lost Revival, Friendly Faux, The Original Soundtrack, The Up All Nights, Coya Hill, and Why Omen",
+        "start_time": "2018-03-17T21:00:00-0400",
+        "id": "1695352770485396"
+    },
+    {
+        "name": "WCBE presents An Intimate Evening with Langhorne Slim w/ Christian Lee H...",
+        "start_time": "2018-03-16T21:00:00-0400",
+        "id": "926161100875360"
+    },
+    {
+        "name": "Gangstagrass",
+        "start_time": "2018-03-15T21:00:00-0400",
+        "id": "698778166983593"
+    },
+    {
+        "name": "Gangstagrass at Rumba Cafe",
+        "start_time": "2018-03-15T21:00:00-0400",
+        "id": "383421705440242"
+    },
+    {
+        "name": "Aaron Carter w/ Amy Guess",
+        "start_time": "2018-03-14T20:00:00-0400",
+        "id": "417561735366337"
+    },
+    {
+        "name": "The Dollyrots",
+        "start_time": "2018-03-11T19:00:00-0400",
+        "id": "177938182946357"
+    },
+    {
+        "name": "Men I Trust",
+        "start_time": "2018-03-10T23:00:00-0500",
+        "id": "330163840835478"
+    },
+    {
+        "name": "Lydia",
+        "start_time": "2018-03-10T20:00:00-0500",
+        "id": "1999433976973854"
+    },
+    {
+        "name": "Joe Pug w/ Split Single's Jason Narducy",
+        "start_time": "2018-03-08T20:00:00-0500",
+        "id": "2008806072726362"
+    },
+    {
+        "name": "Aaron Lee Tasjan & Dylan Leblanc",
+        "start_time": "2018-03-07T20:00:00-0500",
+        "id": "1331216616982794"
+    },
+    {
+        "name": "Rivers and Rust",
+        "start_time": "2018-03-06T20:00:00-0500",
+        "id": "914553112029174"
+    },
+    {
+        "name": "Caroline Rose & The Nude Party",
+        "start_time": "2018-03-05T20:30:00-0500",
+        "id": "145242842829808"
+    },
+    {
+        "name": "Joy Again",
+        "start_time": "2018-03-04T19:00:00-0500",
+        "id": "385594301885338"
+    },
+    {
+        "name": "Anthony Green",
+        "start_time": "2018-03-02T21:00:00-0500",
+        "id": "100446157441039"
+    },
+    {
+        "name": "Jessica Lea Mayfield w/ Mal Blum and The Blums",
+        "start_time": "2018-02-28T20:00:00-0500",
+        "id": "312481895900899"
+    },
+    {
+        "name": "Ratboys",
+        "start_time": "2018-02-27T20:00:00-0500",
+        "id": "565205597149325"
+    },
+    {
+        "name": "Solitaires",
+        "start_time": "2018-02-24T21:00:00-0500",
+        "id": "180078489418118"
+    },
+    {
+        "name": "Hey Monea",
+        "start_time": "2018-02-17T21:00:00-0500",
+        "id": "188818888537846"
+    },
+    {
+        "name": "The Vindys w/ George Barrie Band",
+        "start_time": "2018-02-16T22:00:00-0500",
+        "id": "1660066777366079"
+    },
+    {
+        "name": "The Orphan The Poet & I The Victor w/ Captain Kidd * 90s Kids",
+        "start_time": "2018-02-16T18:30:00-0500",
+        "id": "149541939023354"
+    },
+    {
+        "name": "SIMO",
+        "start_time": "2018-02-15T20:00:00-0500",
+        "id": "344546786030134"
+    },
+    {
+        "name": "Haunted Summer",
+        "start_time": "2018-02-14T20:00:00-0500",
+        "id": "128854681228046"
+    },
+    {
+        "name": "The Ghost of Paul Revere & Parsonsfield",
+        "start_time": "2018-02-13T20:00:00-0500",
+        "id": "196588254233336"
+    },
+    {
+        "name": "Castile Magenta EP Release w/ INBOKEH * Zombie On Radar",
+        "start_time": "2018-02-10T20:00:00-0500",
+        "id": "355920421540036"
+    },
+    {
+        "name": "Dropsickle & Silver Fox & The Hounds",
+        "start_time": "2018-02-09T22:00:00-0500",
+        "id": "2036822673266828"
+    },
+    {
+        "name": "Bava Choco",
+        "start_time": "2018-02-09T19:00:00-0500",
+        "id": "137598110367668"
+    },
+    {
+        "name": "No Relation w/ The Comos",
+        "start_time": "2018-02-08T22:00:00-0500",
+        "id": "157705941548551"
+    },
+    {
+        "name": "Hebdo at Rumba Cafe (Early Happy Hour)",
+        "start_time": "2018-02-08T19:00:00-0500",
+        "id": "550610338621914"
+    },
+    {
+        "name": "Spaceface w/ Fluffer and Classical Baby",
+        "start_time": "2018-02-03T21:00:00-0500",
+        "id": "197013290864120"
+    },
+    {
+        "name": "Love U x The Daytimers",
+        "start_time": "2018-02-02T21:00:00-0500",
+        "id": "158124494797706"
+    },
+    {
+        "name": "Jennifer Maurer & Jesse Henry Duo",
+        "start_time": "2018-02-02T19:00:00-0500",
+        "id": "245585919313844"
+    },
+    {
+        "name": "A Night With J-LIVE feat. J Rawls, Pow Bundy & guests",
+        "start_time": "2018-02-01T21:00:00-0500",
+        "id": "141376903226707"
+    },
+    {
+        "name": "Shredders \"members of Doomtree \" w/ Astronautalis",
+        "start_time": "2018-01-31T21:00:00-0500",
+        "id": "197003890868360"
+    }
+]
